5334cd5cd2d4a70a0d79294206b89229dd1a3dea
faith <faithmueni6@gmail.com>
2023-02-10 22:18:22 +0300

    added twosum

---
+++ twoSum.js
@@ -0,0 +1,28 @@
+//Question #13
+
+//https://leetcode.com/problems/two-sum/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    const hashmap = {};
+      for (let i=0; i<nums.length; i++){
+          const diff = target - nums[i];
+          if (diff in hashmap){
+              return [hashmap[diff],i];
+          }
+          hashmap[nums[i]]=i;
+      }
+
+  return []
+
+
+};
+
+
+
+
+
